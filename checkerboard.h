0981e475076e5bcb09f99038e45e97179a470ae5
Emil Taylor Bye <phptph@gmail.com>
2012-06-23 17:24:50 +0200

    Renamed the bit board type and fixed macros
    The bit board type is now _board

--- checkerboard.h
+++ checkerboard.h
@@ -3,21 +3,21 @@
 #include <stdint.h>
 
 #define POPULATION_COUNT(x) __builtin_popcount(x)
-#define GET_BIT(number, bit) number & (1 << bit)
-#define SET_BIT(number, bit) number | (1 << bit)
-#define UNSET_BIT(number, bit) number & ~(1 << bit)
+#define GET_BIT(number, bit) (number & (1 << bit))
+#define SET_BIT(number, bit) (number | (1 << bit))
+#define UNSET_BIT(number, bit) (number & ~(1 << bit))
 #define POSITION_IS_WHITE(board, x) GET_BIT(board.white, x)
 #define POSITION_IS_BLACK(board, x) GET_BIT(board.black, x)
 #define POSITION_IS_KING(board, x) GET_BIT(board.kings, x)
 #define POSITION_IS_TAKEN(board, x) POSITION_IS_WHITE(board, x) || POSITION_IS_BLACK(board, x)
 
-typedef uint_fast32_t board;
+typedef uint_fast32_t _board;
 
 typedef struct board_struct {
-    board black;
-    board white;
-    board kings;
-    board occupied;
+    _board black;
+    _board white;
+    _board kings;
+    _board occupied;
 } board_t;
 
 board_t input2board(char *);
