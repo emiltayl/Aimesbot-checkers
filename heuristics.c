80f765c9b3c4766dc01dc603e37478ae50ee068f
Emil Taylor Bye <phptph@gmail.com>
2012-07-05 13:53:56 +0200

    Changed the calculation of runaway men

--- heuristics.c
+++ heuristics.c
@@ -19,22 +19,22 @@ heuristic_t calculate_heuristics(board_t board, int isPlayerWhite, int isItPlaye
     score += blackModifier * blackKingCount* 50;
 
     // Runaway man
-    score += ((0x00000010 & board.white) && !(0x00000010 & board.kings) && !(0x00000001 & board.occupied)) * 47 * whiteModifier;
-    score += ((0x01000000 & board.black) && !(0x01000000 & board.kings) && !(0x30000000 & board.occupied)) * 47 * blackModifier;
-    score += ((0x00000020 & board.white) && !(0x00000020 & board.kings) && !(0x00000003 & board.occupied)) * 47 * whiteModifier;
-    score += ((0x02000000 & board.black) && !(0x02000000 & board.kings) && !(0x60000000 & board.occupied)) * 47 * blackModifier;
-    score += ((0x00000040 & board.white) && !(0x00000040 & board.kings) && !(0x00000006 & board.occupied)) * 47 * whiteModifier;
-    score += ((0x04000000 & board.black) && !(0x04000000 & board.kings) && !(0xC0000000 & board.occupied)) * 47 * blackModifier;
-    score += ((0x00000080 & board.white) && !(0x00000080 & board.kings) && !(0x0000000C & board.occupied)) * 47 * whiteModifier;
-    score += ((0x08000000 & board.black) && !(0x08000000 & board.kings) && !(0x80000000 & board.occupied)) * 47 * blackModifier;
-    score += ((0x00000100 & board.white) && !(0x00000100 & board.kings) && !(0x00000011 & board.occupied)) * 44 * whiteModifier;
-    score += ((0x00100000 & board.black) && !(0x00100000 & board.kings) && !(0x31000000 & board.occupied)) * 44 * blackModifier;
-    score += ((0x00000200 & board.white) && !(0x00000200 & board.kings) && !(0x00000067 & board.occupied)) * 44 * whiteModifier;
-    score += ((0x00200000 & board.black) && !(0x00200000 & board.kings) && !(0x73000000 & board.occupied)) * 44 * blackModifier;
-    score += ((0x00000400 & board.white) && !(0x00000400 & board.kings) && !(0x000000CE & board.occupied)) * 44 * whiteModifier;
-    score += ((0x00400000 & board.black) && !(0x00400000 & board.kings) && !(0xE6000000 & board.occupied)) * 44 * blackModifier;
-    score += ((0x00000800 & board.white) && !(0x00000800 & board.kings) && !(0x0000008C & board.occupied)) * 44 * whiteModifier;
-    score += ((0x00800000 & board.black) && !(0x00800000 & board.kings) && !(0x88000000 & board.occupied)) * 44 * blackModifier;
+    score += ((0x00000010 & (board.white & (~board.kings))) && !(0x00000001 & board.occupied)) * 47 * whiteModifier;
+    score += ((0x01000000 & (board.black & (~board.kings))) && !(0x30000000 & board.occupied)) * 47 * blackModifier;
+    score += ((0x00000020 & (board.white & (~board.kings))) && !(0x00000003 & board.occupied)) * 47 * whiteModifier;
+    score += ((0x02000000 & (board.black & (~board.kings))) && !(0x60000000 & board.occupied)) * 47 * blackModifier;
+    score += ((0x00000040 & (board.white & (~board.kings))) && !(0x00000006 & board.occupied)) * 47 * whiteModifier;
+    score += ((0x04000000 & (board.black & (~board.kings))) && !(0xC0000000 & board.occupied)) * 47 * blackModifier;
+    score += ((0x00000080 & (board.white & (~board.kings))) && !(0x0000000C & board.occupied)) * 47 * whiteModifier;
+    score += ((0x08000000 & (board.black & (~board.kings))) && !(0x80000000 & board.occupied)) * 47 * blackModifier;
+    score += ((0x00000100 & (board.white & (~board.kings))) && !(0x00000011 & board.occupied)) * 44 * whiteModifier;
+    score += ((0x00100000 & (board.black & (~board.kings))) && !(0x31000000 & board.occupied)) * 44 * blackModifier;
+    score += ((0x00000200 & (board.white & (~board.kings))) && !(0x00000067 & board.occupied)) * 44 * whiteModifier;
+    score += ((0x00200000 & (board.black & (~board.kings))) && !(0x73000000 & board.occupied)) * 44 * blackModifier;
+    score += ((0x00000400 & (board.white & (~board.kings))) && !(0x000000CE & board.occupied)) * 44 * whiteModifier;
+    score += ((0x00400000 & (board.black & (~board.kings))) && !(0xE6000000 & board.occupied)) * 44 * blackModifier;
+    score += ((0x00000800 & (board.white & (~board.kings))) && !(0x0000008C & board.occupied)) * 44 * whiteModifier;
+    score += ((0x00800000 & (board.black & (~board.kings))) && !(0x88000000 & board.occupied)) * 44 * blackModifier;
 
     // Back row bonus
     score += population_count(0xF0000000 & board.occupied) * 7 * whiteModifier;
